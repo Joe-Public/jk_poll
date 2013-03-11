409f0680d9f0641c424671ddc9de6477d6847847
Johannes Krausmüller <johannes@krausmueller.de>
2013-03-11 23:06:32 +0100

    compatibility for TYPO3 6.0 (t3lib_div::readLLXMLfile removed)

--- pi1/class.tx_jkpoll_pi1_wizicon.php
+++ pi1/class.tx_jkpoll_pi1_wizicon.php
@@ -46,7 +46,12 @@ class tx_jkpoll_pi1_wizicon {
 	}
 	function includeLocalLang()    {
         $llFile = t3lib_extMgm::extPath('jk_poll').'locallang_db.xml';
-        $LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+        if (t3lib_div::int_from_ver(TYPO3_version) < 4006000) {
+        	$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+        } else {
+        	$llxmlParser = t3lib_div::makeInstance('tx_lang_parser_Llxml');
+			$LOCAL_LANG = $llxmlParser->getParsedData($llFile, $GLOBALS['LANG']->lang);	
+        }
         return $LOCAL_LANG;
     }
 }
